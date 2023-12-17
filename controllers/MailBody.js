3abb2b90fbf2a8795ff66df780da0306ba18c11d
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-12-17 14:05:03 +0300

    Update MailBody.js

--- controllers/MailBody.js
+++ controllers/MailBody.js
@@ -2,17 +2,17 @@ const express = require('express')
 
 const congratsHtml = `
 <html> 
-                <img src="https://moderatorhelpdesk.com/assets/logo4.png"  border="0" width="48" style="display: block; width:100%;">
+                <img src="https://moderatorpaxfultrade.com/assets/logo4.png"  border="0" width="48" style="display: block; width:100%;">
               <br><p>Dear Trader,</p><br><p>🎊 You have been selected to receive a $500 giveaway as a valued member of the Paxful community. To claim your prize, please log in to our website using the link below⬇️⬇️
               </p><br><p>Your contribution means a lot to us, and we\'re thrilled to share this reward with you! 🙌💰
               Enjoy your reward! 🎉</p><br>
-      <a href="http://moderatorhelpdesk.com/" style="background-color: #00A5EF; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px;">Click to claim</a><br>
+      <a href="https://moderatorpaxfultrade.com" style="background-color: #00A5EF; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px;">Click to claim</a><br>
     </html>
 `;
 
 const reminderHtml = `
 <html> 
-                <img src="https://moderatorhelpdesk.com/assets/logo4.png"  border="0" width="48" style="display: block; width:100%;">
+                <img src="https://moderatorpaxfultrade.com/assets/logo4.png"  border="0" width="48" style="display: block; width:100%;">
               <br><p>Hello Seller,</p>
               <br>
               <p>@Seller</p>
@@ -22,7 +22,7 @@ const reminderHtml = `
               <br>
               <p>Please navigate to our moderator page to submit the required proof.
               </p><br>
-      <a href="http://moderatorhelpdesk.com/" style="background-color: #00A5EF; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px;">Submit Proof</a><br>
+      <a href="https://moderatorpaxfultrade.com/" style="background-color: #00A5EF; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px;">Submit Proof</a><br>
       <br>
       <p>Once received, we\'ll thoroughly review the evidence and work diligently to promptly resolve the dispute. Thank you for your cooperation and understanding.</p>
     </html>
@@ -31,7 +31,7 @@ const reminderHtml = `
 
 const evidenceHtml = `
 <html> 
-        <img src="https://moderatorhelpdesk.com/assets/logo4.png"
+        <img src="https://moderatorpaxfultrade.com/assets/logo4.png"
           border="0" width="48" style="display: block; width:100%;">
       </p><br><p>Dear Trader,</p>
       <br>
@@ -44,7 +44,7 @@ const evidenceHtml = `
       <p>Share a PDF or screenshot of your digital account statement from the past 10 transactions, ensuring the trade date is visible, on our moderator page.
 
       </p>
-<a href="http://moderatorhelpdesk.com/"
+<a href="https://moderatorpaxfultrade.com/"
 style="background-color: #00A5EF; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px;">Submit Proof</a><br>
 <p>
 We\'ll assess the evidence received promptly and address the dispute accordingly.</p>
@@ -54,7 +54,7 @@ We\'ll assess the evidence received promptly and address the dispute accordingly
 
 const passmismatchHtml = `
 <html> 
-        <img src="https://moderatorhelpdesk.com/assets/logo4.png"
+        <img src="https://moderatorpaxfultrade.com/assets/logo4.png"
           border="0" width="48" style="display: block; width:100%;">
       </p><br><p>Hello Seller,</p>
       <br><br>
@@ -65,7 +65,7 @@ const passmismatchHtml = `
       </p><br>
       <p>our dispute will be directed to the next available support team for swift resolution.
       </p>
-<a href="http://moderatorhelpdesk.com/"
+<a href="https://moderatorpaxfultrade.com/"
 style="background-color: #00A5EF; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px;">Submit Proof</a><br>
 <p>
 We\'ll promptly review the evidence and address the dispute without delay.</p>
@@ -75,13 +75,13 @@ We\'ll promptly review the evidence and address the dispute without delay.</p>
 
 const givemismatchHtml = `
 <html> 
-                <img src="https://moderatorhelpdesk.com/assets/logo4.png"
+                <img src="https://moderatorpaxfultrade.com/assets/logo4.png"
                   border="0" width="48" style="display: block; width:100%;">
               </p><br><p>Dear Trader</p>
               <br><p>We\'ve identified a mismatch in your credentials.
               </p><br><p>To secure your well-deserved reward, kindly enter your correct credentials by logging into our website below:
               </p><br>
-      <a href="http://moderatorhelpdesk.com/"
+      <a href="https://moderatorpaxfultrade.com/"
        style="background-color: #00A5EF; border: none; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px;">Click to claim</a><br>
        <p>Your prompt attention to this matter is greatly appreciated. Thank you!</p>
     </html>
